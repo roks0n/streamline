c9f58c81488116d50df310bdea843210dded9e2b
rhaluzan <haluzan.rok@gmail.com>
2014-06-18 22:08:48 +0200

    wall + restrict /admin to admin roles only

--- apps/admin/__init__.py
+++ apps/admin/__init__.py
@@ -1,4 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, session, redirect, url_for
+import views
 
 admin = Blueprint('admin', __name__, static_folder='static', template_folder='templates')
-import views
+
+
+@admin.before_request
+def restrict_bp_to_admins():
+    if 'admin' not in session.get('role'):
+        return redirect(url_for('site.index'))
